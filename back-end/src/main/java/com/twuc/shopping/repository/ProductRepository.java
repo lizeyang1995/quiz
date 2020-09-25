b6f58b07f5629978122cb8c3de6d2024b5065c38
lizeyang1995 <424011527@qq.com>
2020-09-25 15:06:08 +0800

    [lizeyang] feat: create product repository

---
+++ back-end/src/main/java/com/twuc/shopping/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.twuc.shopping.repository;
+
+import com.twuc.shopping.po.ProductPo;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<ProductPo, Integer> {
+    @Override
+    List<ProductPo> findAll();
+}
