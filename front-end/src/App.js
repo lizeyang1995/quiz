26a4b7a133d0137255a83b060d3c7e37f4a5543d
lizeyang1995 <424011527@qq.com>
2020-09-25 14:44:36 +0800

    [lizeyang] feat: add OrderList and AddProduct.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/home/Home';
+import OrderList from './components/orderList/OrderList';
+import AddProduct from './components/add/AddProduct';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Header />
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/orders' component={OrderList} />
+          <Route exact path='/product' component={AddProduct} />
         </Switch>
       </div>
     </Router>
