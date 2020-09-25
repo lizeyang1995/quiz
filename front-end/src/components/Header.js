ba23905adb90b7613fd0161d507289eca6a8ac8f
lizeyang1995 <424011527@qq.com>
2020-09-25 13:58:50 +0800

    [lizeyang] feat: create Header

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <p>
+          <Link className='link' to='/'>商城</Link>
+        </p>
+        <p>
+          <Link className='link' to='/order'>订单</Link>
+        </p>
+        <p>
+          <Link className='link' to='/product'>添加商品</Link>
+        </p>
+      </div>
+    )
+  }
+}
+export default Header;
