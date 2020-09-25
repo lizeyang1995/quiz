479ec4dfa09d06e8b7f072856d9fee8175541372
lizeyang1995 <424011527@qq.com>
2020-09-25 17:59:27 +0800

    [lizeyang] feat: add a tbody and theader

--- front-end/src/components/orderList/OrderList.js
+++ front-end/src/components/orderList/OrderList.js
@@ -23,13 +23,16 @@ class OrderList extends Component {
     return (
       <div className="orders">
         <table  width="500" height="50" align="center" >
-          <tr>
-              <th>名字</th>
-              <th>单价</th>
-              <th>数量</th>
-              <th>单位</th>
-              <th>操作</th>
-          </tr>
+          <thead>
+            <tr>
+                <th>名字</th>
+                <th>单价</th>
+                <th>数量</th>
+                <th>单位</th>
+                <th>操作</th>
+            </tr>
+          </thead>
+          <tbody>
           {this.state.product.map((item) => (
             <tr key={item.name}>
               <th>{item.name}</th>
@@ -39,6 +42,7 @@ class OrderList extends Component {
               <th>{!this.state.noProduct && <button>删除</button>}</th>
             </tr>
           ))}
+          </tbody>
         </table>
         {this.state.noProduct && 
           <p style={{color:'red'}}>暂无订单，返回<Link to='/'>商城页面</Link>继续购买</p>
