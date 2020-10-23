9746c59dfdf9265fa3b266a471305eb110e8d574
lizeyang1995 <424011527@qq.com>
2020-10-23 11:55:15 +0800

    [lizeyang] feat: add a test, should get all orders

--- back-end/src/test/java/com/twuc/shopping/api/OrderControllerTest.java
+++ back-end/src/test/java/com/twuc/shopping/api/OrderControllerTest.java
@@ -2,6 +2,7 @@ package com.twuc.shopping.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.twuc.shopping.domain.Order;
+import com.twuc.shopping.po.OrderPo;
 import com.twuc.shopping.repository.OrderRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -63,5 +67,22 @@ public class OrderControllerTest {
         String jsonString = objectMapper.writeValueAsString(order);
         mockMvc.perform(post("/order").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void should_get_all_orders() throws Exception {
+        OrderPo order = OrderPo.builder()
+                .name("可乐")
+                .price(BigDecimal.valueOf(5.00))
+                .unit("瓶")
+                .count(BigInteger.valueOf(1))
+                .build();
+        orderRepository.save(order);
+        mockMvc.perform(get("/orders"))
+                .andExpect(jsonPath("$[0].name", is("可乐")))
+                .andExpect(jsonPath("$[0].price", is(5.00)))
+                .andExpect(jsonPath("$[0].unit", is("瓶")))
+                .andExpect(jsonPath("$[0].count", is(1)))
+                .andExpect(status().isOk());
     }
 }
