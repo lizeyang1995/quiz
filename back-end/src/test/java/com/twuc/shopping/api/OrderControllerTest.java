8105c6643550c16c7b21b0e776285c65033021b8
lizeyang1995 <424011527@qq.com>
2020-10-23 10:46:59 +0800

    [lizeyang] feat: add a test, add order

---
+++ back-end/src/test/java/com/twuc/shopping/api/OrderControllerTest.java
@@ -0,0 +1,40 @@
+package com.twuc.shopping.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twuc.shopping.domain.Order;
+import com.twuc.shopping.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class OrderControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    OrderRepository orderRepository;
+    @BeforeEach
+    void setUp() {
+        orderRepository.deleteAll();
+    }
+
+    @Test
+    void should_add_a_order() throws Exception {
+        Order order = Order.builder()
+                            .name("可乐")
+                            .price(BigDecimal.valueOf(5.00))
+                            .unit("瓶")
+                            .count(BigDecimal.valueOf(1))
+                            .build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(order);
+        mockMvc.perform(post("/order").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
