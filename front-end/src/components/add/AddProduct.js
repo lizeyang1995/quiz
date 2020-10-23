df5c24c2b0b38e0a694ec4298d9a8ec5460f610c
lizeyang1995 <424011527@qq.com>
2020-10-23 09:27:28 +0800

    [lizeyang] feat: add product in database

--- front-end/src/components/add/AddProduct.js
+++ front-end/src/components/add/AddProduct.js
@@ -1,43 +1,73 @@
 import React, {Component} from 'react';
 import './AddProduct.css';
 
+let myHeaders = new Headers({
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+});
+let api = 'http://localhost:8080/product';
 class AddProduct extends Component {
   constructor(props) {
     super(props)
     this.state = {
       name:'',
-      price:0,
+      price:'',
       unit:'',
       url:''
     }
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  submit = () => {
+    fetch(api,{
+      method:'POST',
+      headers: myHeaders,
+      mode: 'cors',
+      body: JSON.stringify(this.state)
+  })
+  .then((res)=>res.json())
+  .then((data)=>{
+    console.log("")
+  })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+    
+
   render() {
     return (
       <div className='background'>
-        <form className='add-product'>
+        <form className='add-product' onSubmit={this.handleSubmit}>
           <h1>添加商品</h1>
           <section className='name'>
             <span>*</span>
             <h3>名称:</h3>
-            <input placeholder='名称'></input>
+            <input placeholder='名称' onChange={this.handleChange} name='name' value={this.state.name}></input>
           </section>
           <section className='price'>
             <span>*</span>
             <h3>价格:</h3>
-            <input placeholder='价格'></input>
+            <input placeholder='价格' onChange={this.handleChange} name='price' value={this.state.price}></input>
           </section>
           <section className='unit'>
             <span>*</span>
             <h3>单位:</h3>
-            <input placeholder='单位'></input>
+            <input placeholder='单位' onChange={this.handleChange} name='unit' value={this.state.unit}></input>
           </section>
-          <section className='price'>
+          <section className='url'>
             <span>*</span>
             <h3>图片:</h3>
-            <input placeholder='URL'></input>
+            <input placeholder='URL' onChange={this.handleChange} name='url' value={this.state.url}></input>
           </section>
-          <button disabled={!this.state.name || !this.state.price || !this.state.unit || !this.state.url}>提交</button>
+          <button disabled={!this.state.name || !this.state.price || !this.state.unit || !this.state.url} onClick={this.submit}>提交</button>
         </form>
         <footer>TW Mall 2018 Create by ForCheng</footer>
       </div>
